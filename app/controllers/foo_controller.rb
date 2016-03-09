78d94b6358bb2df66a8c609e1c7ed30d567ce2cf
Filipe Abreu <filabreu@gmail.com>
2016-03-09 00:47:35 -0300

    Add FooController and BarController. Add restful routes for FooController and BarController actions. Add a before_filter to authenticate all FooController and BarController actions.

---
+++ app/controllers/foo_controller.rb
@@ -0,0 +1,25 @@
+class FooController < ApplicationController
+
+  before_action :authenticate
+
+  def index
+    head :ok
+  end
+
+  def show
+    head :ok
+  end
+
+  def create
+    head :ok
+  end
+
+  def update
+    head :ok
+  end
+
+  def destroy
+    head :ok
+  end
+
+end
