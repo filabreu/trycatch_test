d30cf407015ad35b2ad8f862be2a31756e3a9a2a
Filipe Abreu <filabreu@gmail.com>
2016-03-09 00:36:26 -0300

    Add http authentication to ApplicationController. Add password authentication for User model.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+    def authenticate
+      user = authenticate_user
+      user ? @current_user = user : request_http_basic_authentication
+    end
+
+    def authenticate_user
+      authenticate_with_http_basic do |username, password|
+        user = User.find_by_username(username)
+        user && user.authenticate(password)
+      end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+
   enum roles: [:guest, :user, :admin]
 
   validates :username, uniqueness: true
