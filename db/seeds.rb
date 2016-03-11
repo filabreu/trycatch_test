6dbf6da37093f16e9292992d2aa544e0712b37d8
Filipe Abreu <filabreu@gmail.com>
2016-03-11 13:47:25 -0300

    Provide seed data for Foo and Bar records, for both user and admin User roles.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.create(username: "guest", password: "password", role: :guest)
+user = User.create(username: "user", password: "password", role: :user)
+guest = User.create(username: "admin", password: "password", role: :admin)
+
+user_foo = Foo.create(title: "User Foo", user: user)
+Bar.create(title: "User Bar", foo: user_foo)
+
+admin_foo = Foo.create(title: "Admin Foo", user: admin)
+Bar.create(title: "Admin Bar", foo: admin_foo)
